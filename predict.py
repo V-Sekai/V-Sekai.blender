c06d13f47a8c68cd845d9c21f4d0a63882c1493e
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-09-03 13:15:08 -0700

    Restore bsynth.

--- predict.py
+++ predict.py
@@ -1,5 +1,7 @@
-import bpy
-from cog import BasePredictor, Input, Path, BaseModel
+#!/usr/bin/env python3
+
+from cog import BasePredictor, Input, Path, BaseModel, File
+import blendersynth as bsyn
 
 class Output(BaseModel):
     response: Path
@@ -9,36 +11,38 @@ class Predictor(BasePredictor):
         pass
 
     def predict(self, input_scene: Path = Input(description="Input scene")) -> Output:
-        bpy.ops.wm.open_mainfile(filepath=str(input_scene))
-        bpy.context.scene.render.resolution_x = 1024
-        bpy.context.scene.render.resolution_y = 1024
-        bpy.context.scene.cycles.samples = 10
+        bsyn.run_this_script(debug = False)
+        bsyn.load_blend(str(image_path))  # load from scene
+        bsyn.render.set_resolution(1024, 1024)
+        bsyn.render.set_cycles_samples(10)
 
-        monkey = bpy.data.objects.get('Monkey')
-        cube = bpy.data.objects.get('Cube')
+        monkey = bsyn.Mesh.from_scene('Monkey')
+        cube = bsyn.Mesh.from_scene('Cube')
 
-        # Create Compositor Nodes
-        bpy.context.scene.use_nodes = True
-        tree = bpy.context.scene.node_tree
-        links = tree.links
-        nodes = tree.nodes
-        for node in nodes:
-            nodes.remove(node)
+        comp = bsyn.Compositor()
+        bounding_box_visual = comp.get_bounding_box_visual()
+        keypoints_visual = comp.get_keypoints_visual(marker='x')  # Create a visual of keypoints
+        axis_visual = comp.get_axes_visual()
+        combined_visual = comp.stack_visuals(bounding_box_visual, keypoints_visual, axis_visual)
 
-        render_layers = nodes.new('CompositorNodeRLayers')
-        output = nodes.new('CompositorNodeComposite')
+        comp.define_output('Image', directory='pose')
+        comp.define_output(combined_visual, file_name='visual', directory='pose')
 
-        # Connect Nodes
-        links.new(render_layers.outputs[0], output.inputs[0])
+        objects = [monkey, cube]
+        bounding_boxes = bsyn.annotations.bounding_boxes(objects)
 
-        # Render Image
-        bpy.ops.render.render()
+        keypoints = bsyn.annotations.keypoints.project_keypoints([obj.centroid() for obj in objects])
 
-        image_path = bpy.context.scene.render.filepath + "rendered_image.png"
+        axes = bsyn.annotations.get_multiple_axes(objects)
 
-        return Output(response=Path(image_path))
+        overlay_kwargs = dict(BBox=bounding_boxes, Keypoints=keypoints, Axes=axes)
+        comp.render(overlay_kwargs=overlay_kwargs)
+        image_path = bsyn.render.get_output_path('Image', file_name='rendered_image.png')
+
+        return Output(image=Path(image_path))
 
 if __name__ == "__main__":
     predictor = Predictor()
     predictor.setup()
-    result = predictor.predict(Path("thirdparty/capturing_pose.blend"))
+    result = predictor.predict("thirdparty/capturing_pose.blend")
+
